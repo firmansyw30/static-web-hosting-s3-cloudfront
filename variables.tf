b52077dc86a8ed87edcc57052dea11acc77bea35
Firmansyah Wicaksono <firmansyahwicaksono30@gmail.com>
2026-02-26 15:19:10 +0700

    Add new variables for CloudFront configuration

--- variables.tf
+++ variables.tf
@@ -3,3 +3,27 @@ variable "bucket_name" {
   type        = string
   default     = "your-bucket-name"
 }
+
+variable "my_domain" {
+  description = "The domain name for the CloudFront distribution."
+  type        = string
+  default     = "your-domain.name"
+}
+
+variable "cloudfront_alias" {
+  description = "The CloudFront alias subdomain."
+  type        = string
+  default     = "your-cloudfront-alias"
+}
+
+variable "content_path" {
+  description = "Path to the static content files to upload."
+  type        = string
+  default     = "the-content-path"
+}
+
+variable "environment" {
+  description = "Environment name for tags."
+  type        = string
+  default     = "your-environment"
+}
